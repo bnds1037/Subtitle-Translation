858a18c09ab400a28434ae7ddacd61631876a7bf
LiuMinghao <Liuminghao101@163.com>
2026-03-24 21:29:29 +0800

    Initial Commit

---
+++ Translation/TranslationApp.swift
@@ -0,0 +1,17 @@
+//
+//  TranslationApp.swift
+//  Translation
+//
+//  Created by 刘明浩 on 2026/3/24.
+//
+
+import SwiftUI
+
+@main
+struct TranslationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
